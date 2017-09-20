6380ebd3906bea6a9a7ae43abb3e2e6ed449bbc5
Stéphane Goudeau <stephgou@microsoft.com>
2017-09-20 13:26:14 +0200

    Basic intent triggering

--- app.js
+++ app.js
@@ -17,16 +17,221 @@ var connector = new builder.ChatConnector({
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
-var bot = new builder.UniversalBot(connector);
+//var bot = new builder.UniversalBot(connector);
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send('Sorry, I did not understand \'%s\'. Type \'help\' if you need assistance.', session.message.text);
+});
 server.post('/api/messages', connector.listen());
 
+//=========================================================
+// Bots Global Actions
+//=========================================================
+
+// bot.endConversationAction('goodbye', 'Goodbye :)', { matches: /^goodbye/i });
+
+// bot.beginDialogAction('help', '/help', { matches: /^help/i });
+// bot.beginDialogAction('reset', '/reset', { matches: /^reset/i });
 
 //=========================================================
 // Bots Dialogs
 //=========================================================
 
-bot.dialog('/', function (session) { 
+var model = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/371e5058-5522-44c7-9fce-759c19b7ff9a?subscription-key=cbf1bd099b9e40aa865124d487346e04&timezoneOffset=0&verbose=true&q=';
+var recognizer = new builder.LuisRecognizer(model);
+bot.recognizer(recognizer);
 
-	session.send('Hello World'); 
+var intents = new builder.IntentDialog({ recognizers: [recognizer] });
 
+bot.dialog('Create', [
+    function (session, args, next) {
+        //session.send('Create - We are analyzing your message: \'%s\'', session.message.text);
+        // try extracting entities
+
+        var serviceType = builder.EntityRecognizer.findEntity(args.intent.entities,'SERVICE-TYPE');
+        if (serviceType) {
+            // serviceType entity detected, continue to next step
+            serviceTypeEntity = serviceType.entity;
+
+            //session.send(serviceTypeEntity, session.message.text);
+
+
+            if (serviceTypeEntity.trim()== 'vm') {
+                //session.send('vm', session.message.text);
+                var osName = builder.EntityRecognizer.findEntity(args.intent.entities,'OS-NAME');
+                if (osName) {
+                    // serviceType entity detected, continue to next step
+                    session.send(osName.entity, session.message.text);
+                }
+                else {
+                    builder.Prompts.text(session, "Quel Système d'exploitation ?");
+                }                               
+            }
+            else if (serviceTypeEntity.trim()== 'appservice') {
+                session.send('appservice', session.message.text);
+                
+            }
+            else if (serviceTypeEntity.trim()== 'sqldatabase') {
+                
+            }                   
+        }
+        session.endDialog();
+    }
+]).triggerAction({
+    matches: 'Create',
+    onInterrupted: function (session) {
+        session.send('Interruption Create');
+    }
 });
+
+
+
+bot.dialog('Stop', [
+    function (session, args, next) {
+        session.send('Stop - We are analyzing your message: \'%s\'', session.message.text);
+        // try extracting entities
+
+        // End
+        session.endDialog();
+    }
+]).triggerAction({
+    matches: 'Stop',
+    onInterrupted: function (session) {
+        session.send('Interruption Stop');
+    }
+});
+
+bot.dialog('Start', [
+    function (session, args, next) {
+        session.send('Start - We are analyzing your message: \'%s\'', session.message.text);
+        // try extracting entities
+
+        // End
+        session.endDialog();
+    }
+]).triggerAction({
+    matches: 'Start',
+    onInterrupted: function (session) {
+        session.send('Interruption Start');
+    }
+});
+
+bot.dialog('Restart', [
+    function (session, args, next) {
+        session.send('Restart - We are analyzing your message: \'%s\'', session.message.text);
+        // try extracting entities
+
+        // End
+        session.endDialog();
+    }
+]).triggerAction({
+    matches: 'Restart',
+    onInterrupted: function (session) {
+        session.send('Interruption Restart');
+    }
+});
+
+
+bot.dialog('Test', [
+    function (session, args, next) {
+        session.send('Test - We are analyzing your message: \'%s\'', session.message.text);
+        // try extracting entities
+
+        // End
+        session.endDialog();
+    }
+]).triggerAction({
+    matches: 'Test',
+    onInterrupted: function (session) {
+        session.send('Interruption Test');
+    }
+});
+
+bot.dialog('List', [
+    function (session, args, next) {
+        session.send('List - We are analyzing your message: \'%s\'', session.message.text);
+        // try extracting entities
+
+        // End
+        session.endDialog();
+    }
+]).triggerAction({
+    matches: 'List',
+    onInterrupted: function (session) {
+        session.send('Interruption List');
+    }
+});
+
+intents.onDefault(builder.DialogAction.send("Je ne comprends pas !"));
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+// bot.dialog('/', [
+//     function (session, args, next) {
+//         if (!session.userData.name) {
+//             session.beginDialog('/profile');
+//         } else {
+//             next();
+//         }
+//     },
+//     function (session, results, next) {
+//         session.send('Hello %s!', session.userData.name);
+// 				session.send('Comment puis-je vous aider?');
+// 				session.beginDialog('/cortana');
+//     }
+// ]);
+
+
+// bot.dialog('/profile', [
+// 		function (session) {
+//         session.beginDialog('/picture');
+//     },
+//     function (session) {
+//         builder.Prompts.text(session, "Hello... What's your name?");
+//     },
+//     function (session, results) {
+//         session.userData.name = results.response;
+//         builder.Prompts.number(session, "Hi " + results.response + ", How many years have you been coding?");
+//     },
+//     function (session, results) {
+//         session.userData.coding = results.response;
+//         builder.Prompts.choice(session, "What language do you code Node using?", ["JavaScript", "CoffeeScript", "TypeScript"]);
+//     },
+//     function (session, results) {
+//         session.userData.language = results.response.entity;
+//         session.send("Got it... " + session.userData.name +
+//             " you've been programming for " + session.userData.coding +
+//             " years and use " + session.userData.language + ".");
+//         session.endDialog();
+//     }
+// ]);
+
+
+
+// bot.dialog('/picture', [
+//     function (session) {
+//         var msg = new builder.Message(session)
+//             .attachments([{
+//                 contentType: "image/jpeg",
+//                 contentUrl: "http://www.theoldrobots.com/images62/Bender-18.JPG"
+//             }]);
+//         session.endDialog(msg);
+//     }
+// ]);
+
+// bot.dialog('/cortana', intents);
+
+// bot.dialog('/help', [
+//     function (session) {
+//         session.endDialog("Global commands that are available anytime:\n\n* goodbye - End this conversation.\n* help - Displays these commands.");
+//     }
+// ]);
+
+// bot.dialog('/reset', [
+//     function (session) {
+//         session.userData.name = null;
+//         session.send('I have reset the profile.');
+//         session.endDialog();
+//     }
+// ]);
